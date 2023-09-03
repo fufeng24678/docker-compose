ae9ca65ff3f8930d1c01a926e26c5d18c1862033
fufeng24678 <fufeng24678@163.com>
2023-09-03 16:51:53 +0800

    improvement: 修改挂载目录结构及内存大小

--- rocketmq4-3m-3s/rocketmq-namesrv/init.sh
+++ rocketmq4-3m-3s/rocketmq-namesrv/init.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 USER_ID=3000
 GROUP_ID=3000
 
-mkdir -p ./logs
-chown -R $USER_ID:$GROUP_ID ./logs
+mkdir -p ./rocketmq/logs
+chown -R $USER_ID:$GROUP_ID ./rocketmq
 
 echo 'Rocketmq-namesrv: Init Success !'
